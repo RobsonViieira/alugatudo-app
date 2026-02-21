3f326d45f7cb05e5d500d66b2d6cc3a8dea1e315
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 16:52:29 -0300

    Criar lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AlugaTudoApp());
+}
+
+class AlugaTudoApp extends StatelessWidget {
+  const AlugaTudoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'AlugaTudo',
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AlugaTudo'),
+      ),
+      body: const Center(
+        child: Text(
+          'Bem-vindo ao AlugaTudo 🚀',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
