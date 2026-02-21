0df69ba76d00760704a7134ae790afc31b699dcf
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 16:57:50 -0300

    Atualizar o home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'add_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,13 +7,70 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: const Text('AlugaTudo'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddItemScreen(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text(
-          'Bem-vindo ao AlugaTudo 🚀',
-          style: TextStyle(fontSize: 22),
+
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: const [
+
+          ItemCard(
+            nome: 'PlayStation 5',
+            preco: 'R\$ 50 / dia',
+          ),
+
+          ItemCard(
+            nome: 'Furadeira',
+            preco: 'R\$ 15 / dia',
+          ),
+
+          ItemCard(
+            nome: 'Bicicleta',
+            preco: 'R\$ 20 / dia',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+
+  final String nome;
+  final String preco;
+
+  const ItemCard({
+    super.key,
+    required this.nome,
+    required this.preco,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: const Icon(Icons.inventory),
+        title: Text(nome),
+        subtitle: Text(preco),
+        trailing: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Alugar'),
         ),
       ),
     );
