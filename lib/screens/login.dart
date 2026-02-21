e98dd0140d9efa932d602c719620646355426bc5
Robson Vieira <63127021+RobsonViieira@users.noreply.github.com>
2026-02-21 17:03:38 -0300

    Atualizar o login.dart
    aguardando teste

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -7,14 +7,24 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar(
         title: const Text('AlugaTudo - Login'),
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
+            const Icon(
+              Icons.inventory,
+              size: 80,
+              color: Colors.blue,
+            ),
+
+            const SizedBox(height: 20),
 
             TextField(
               decoration: const InputDecoration(
@@ -35,12 +45,14 @@ class LoginScreen extends StatelessWidget {
 
             ElevatedButton(
               onPressed: () {
+
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const HomeScreen(),
                   ),
                 );
+
               },
               child: const Text('Entrar'),
             ),
